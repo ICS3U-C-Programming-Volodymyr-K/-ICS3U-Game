5bd267afd90535dba6d9ce2be6f28eef65e5cd51
Volodymyr Kryzhanovskyi <volodymyr.kryzhnovskyi@stu.ocsb.ca>
2025-05-26 17:08:06 +0000

    First push, basically a bald code for now.

---
+++ code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Created By: Volodymyr Kryzhanovskyi
+# Date: 05, 25, 2025
+# This program is a game.
+
+import ugame
+
+import stage
+
+
+def scene():
+    #This function is a scene of game
+    print("\n\n\n") #displays three blank lines
+    print("Hello, Vlad!")
+    #makes infinite loop
+    while True:
+        pass #placeholder for now
+if __name__ == "__main__":
+    scene()
