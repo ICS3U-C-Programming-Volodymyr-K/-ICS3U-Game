213f1714a503a49aecfdc96eaf0c9775ba207191
Volodymyr Kryzhanovskyi <volodymyr.kryzhnovskyi@stu.ocsb.ca>
2025-05-30 16:52:29 +0000

    Update for second update.

--- code.py
+++ code.py
@@ -7,18 +7,17 @@ import stage
 
 
 def game_scene():
-    #Access the image from the files with the use of bank
-    image_bank_background = stage.Bank.from_bmp16(
-        "space_aliens_background.bmp")
-    #Sizes the image and participates in display of image due to grid
+    # Access the image from the files with the use of bank
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    # Sizes the image and participates in display of image due to grid
     background = stage.Grid(image_bank_background, 10, 8)
-    #Sets the background and displays it on frame rate of 60
+    # Sets the background and displays it on frame rate of 60
     game = stage.Stage(ugame.display, 60)
-    #Arrays create a nice flow for image to display
+    # Arrays create a nice flow for image to display
     game.layers = [background]
-    #Render block
+    # Render block
     game.render_block()
-    #Place holder for now
+    # Place holder for now
     while True:
         pass
 
