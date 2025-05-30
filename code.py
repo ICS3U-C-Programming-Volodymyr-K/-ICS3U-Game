d1f7e593a80be95936a210f48d8f4990b046785e
Volodymyr Kryzhanovskyi <volodymyr.kryzhnovskyi@stu.ocsb.ca>
2025-05-30 16:51:18 +0000

    Second update for code.

--- code.py
+++ code.py
@@ -2,18 +2,26 @@
 # Created By: Volodymyr Kryzhanovskyi
 # Date: 05, 25, 2025
 # This program is a game.
-
 import ugame
-
 import stage
 
 
-def scene():
-    #This function is a scene of game
-    print("\n\n\n") #displays three blank lines
-    print("Hello, Vlad!")
-    #makes infinite loop
+def game_scene():
+    #Access the image from the files with the use of bank
+    image_bank_background = stage.Bank.from_bmp16(
+        "space_aliens_background.bmp")
+    #Sizes the image and participates in display of image due to grid
+    background = stage.Grid(image_bank_background, 10, 8)
+    #Sets the background and displays it on frame rate of 60
+    game = stage.Stage(ugame.display, 60)
+    #Arrays create a nice flow for image to display
+    game.layers = [background]
+    #Render block
+    game.render_block()
+    #Place holder for now
     while True:
-        pass #placeholder for now
+        pass
+
+
 if __name__ == "__main__":
-    scene()
+    game_scene()
